c4f153b581d42edbafd9261b42609bbfe7d694b3
rboudrouss <r.boudrouss@gmail.com>
2026-03-29 23:57:33 +0200

    feat: automatically set realtive href if githubci

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const owner = process.env.GITHUB_REPOSITORY_OWNER;
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jsulpis.github.io",
-	base: "/slides-template",
+  site: owner ? `https://${owner}.github.io` : undefined,
+  base: repo ? `/${repo}` : undefined,
 });
